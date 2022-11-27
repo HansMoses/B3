4f1cddec0d21ef3c5ac63f95d441bd6522d3e05a
NeedCuan's <111254951+NooobUser@users.noreply.github.com>
2022-11-27 20:41:07 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Ubah Disini
+global.ownerNumber = ['6285859202162']
+global.ownerName = 'Ferlito'
+global.packname = 'NC BOT'
+global.author = 'Ferlito | 62 858 592 021 62' //Ilangin Prefix Yang '' Kalau Gamau No Prefix
+global.sessionName = 'sesi'
+
+//Message Nya Ubah Disini
+global.mess = {
+admin: 'You Are Not Admin!',
+botAdmin: 'Bot Not Admin!',
+botOwner: 'You Are Not My Owner!',
+group: 'Only Group!',
+private: 'Only Private Chat',
+wait: 'Loading...',
+done: 'Done!'
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update'${__filename}'`))
+delete require.cache[file]
+require(file)
+})
